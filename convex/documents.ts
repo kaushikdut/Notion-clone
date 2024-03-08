096a5b80747689705f3a8935b5c4524db21258e1
Koushik Dutta <duttakoushik779@gmail.com>
2024-03-08 20:40:27 +0530

    fourth commit

--- convex/documents.ts
+++ convex/documents.ts
@@ -4,14 +4,28 @@ import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 import { error } from "console";
 
-export const get = query({
-  handler: async (ctx) => {
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id("document")),
+  },
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-    if (!identity) {
-      throw new Error("Not Authenticated");
-    }
 
-    const documents = await ctx.db.query("document").collect();
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("document")
+      .withIndex("by_user_parent", (q) => {
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument);
+      })
+      .filter((q) => {
+        q.eq(q.field("isArchived"), false);
+      })
+      .order("desc")
+      .collect();
     return documents;
   },
 });
