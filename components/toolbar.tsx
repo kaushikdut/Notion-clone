8b7e98e48c658151c65b2228d27d661e46370004
Koushik Dutta <duttakoushik779@gmail.com>
2024-04-08 21:04:27 +0530

    Commit

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,15 +1,52 @@
 "use client";
+import { ElementRef, useRef, useState } from "react";
+import { ImageIcon, Smile, X } from "lucide-react";
 
 import { IconPicker } from "@/components/icon-picker";
 import { Button } from "@/components/ui/button";
+
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
-import { ImageIcon, Smile, X } from "lucide-react";
+import { useMutation } from "convex/react";
+import TextareaAutosize from "react-textarea-autosize";
 
 interface ToolbarProps {
   initialData: Doc<"document">;
   preview?: boolean;
 }
 const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
@@ -29,7 +66,7 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
       {!!initialData.icon && preview && (
         <p className="text-6xl pt-6">{initialData.icon}</p>
       )}
-      <div className="opacity-100 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+      <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
         {!initialData.icon && !preview && (
           <IconPicker asChild onChange={() => {}}>
             <Button
@@ -54,6 +91,23 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
           </Button>
         )}
       </div>
+      {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.5x] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+        >
+          {initialData.title}
+        </div>
+      )}
     </div>
   );
 };
